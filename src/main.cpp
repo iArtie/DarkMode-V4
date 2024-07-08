ab9da800a5c5de8f89c4cc07945bdcbfb7e2f7ba
BitZ <arturo.chavez92u@std.uni.edu.ni>
2024-07-07 23:20:41 -0600

    lol

--- src/main.cpp
+++ src/main.cpp
@@ -756,7 +756,7 @@ class $modify(LevelSelectLayer)
 		auto m_pGround02Sprite = static_cast<CCSpriteBatchNode*>(groundLayer->getChildByID("ground-sprites-2"));
 
 
-		CCArray* children = nullptr;  // Inicializamos children a nullptr
+		CCArray* children = nullptr;  
 
 		for (int i = 0; i < m_pGround01Sprite->getChildren()->count(); ++i) {
 			if (m_pGround01Sprite != nullptr) {
